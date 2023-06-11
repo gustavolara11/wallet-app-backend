1bd6a6fec3c94e3e38e3adf059d2fdbb8601d82f
gustavolara11 <gustavex2007@gmail.com>
2023-06-11 11:59:15 -0300

    feat: implement delete category

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
+const findOne = (id) => {
+  return (query = {
+    name: "fetch-category",
+    text: "SELECT * FROM categories WHERE id = $1",
+    values: [Number(id)],
+  });
+};
+
 router.get("/", (req, res) => {
   db.query("SELECT * FROM categories", (error, response) => {
     if (error) {
@@ -31,4 +39,29 @@ router.post("/", (req, res) => {
   });
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "Id param is mandatory" });
+    }
+    const query = findOne(id);
+    const category = await db.query(query);
+
+    if (!category.rows[0]) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    const text = "DELETE FROM categories WHERE id=$1 RETURNING *";
+    const value = [Number(id)];
+    const deleteResponse = await db.query(text, value);
+    if (!category.rows[0]) {
+      return res.status(400).json({ error: "Category not deleted." });
+    }
+    return res.status(200).json(deleteResponse.rows);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 module.exports = router;
